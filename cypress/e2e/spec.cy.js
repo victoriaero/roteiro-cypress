bec79fbf901baf34ecb0a650667f1e0fe3615ca5
Victoria Estanislau <52786719+victoriaero@users.noreply.github.com>
2025-06-08 18:09:08 -0300

    add

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,68 +4,141 @@ describe('TODOMvc App', () => {
   })
 
   it('Insere uma tarefa', () => {
-    cy.visit(''); 
+    cy.visit('')
 
     cy.get('[data-cy=todo-input]')
-      .type('TP2 de Engenharia de Software{enter}');
+      .type('TP2 de Engenharia de Software{enter}')
 
     cy.get('[data-cy=todos-list]')
       .children()
-      .should('have.length', 1) 
+      .should('have.length', 1)
       .first()
-      .should('have.text', 'TP2 de Engenharia de Software'); 
-  });
+      .should('have.text', 'TP2 de Engenharia de Software')
+  })
 
   it('Insere e deleta uma tarefa', () => {
-    cy.visit('');
+    cy.visit('')
 
     cy.get('[data-cy=todo-input]')
-      .type('TP2 de Engenharia de Software{enter}');
+      .type('TP2 de Engenharia de Software{enter}')
 
     cy.get('[data-cy=todos-list]')
       .children()
-      .should('have.length', 1);
+      .should('have.length', 1)
 
     cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
       .invoke('show')
-      .click();
+      .click()
 
     cy.get('[data-cy=todos-list]')
       .children()
-      .should('have.length', 0);
-  });
+      .should('have.length', 0)
+  })
 
   it('Filtra tarefas completas e ativas', () => {
-    cy.visit(''); 
+    cy.visit('')
 
     cy.get('[data-cy=todo-input]')
       .type('TP2 de ES{enter}')
-      .type('Prova de ES{enter}');
+      .type('Prova de ES{enter}')
 
     cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
       .first()
-      .click();
+      .click()
 
-    cy.get('[data-cy=filter-active-link')
-      .click();
+    cy.get('[data-cy=filter-active-link]')
+      .click()
     cy.get('[data-cy=todos-list]')
       .children()
       .should('have.length', 1)
       .first()
-      .should('have.text', 'Prova de ES');
+      .should('have.text', 'Prova de ES')
 
-    cy.get('[data-cy=filter-completed-link')
-      .click();
+    cy.get('[data-cy=filter-completed-link]')
+      .click()
     cy.get('[data-cy=todos-list]')
       .children()
       .should('have.length', 1)
       .first()
-      .should('have.text', 'TP2 de ES');
+      .should('have.text', 'TP2 de ES')
 
-    cy.get('[data-cy=filter-all-link')
-      .click();
+    cy.get('[data-cy=filter-all-link]')
+      .click()
     cy.get('[data-cy=todos-list]')
       .children()
-      .should('have.length', 2);
-  });
-});
+      .should('have.length', 2)
+  })
+
+  it('Marca e desmarca uma tarefa', () => {
+    cy.visit('')
+
+    cy.get('[data-cy=todo-input]')
+      .type('Ler Cap.-8 do livro{enter}')
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('not.have.class', 'completed')
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click()
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('have.class', 'completed')
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click()
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('not.have.class', 'completed')
+  })
+
+  it('Remove a primeira tarefa sem afetar as demais', () => {
+    cy.visit('')
+
+    cy.get('[data-cy=todo-input]')
+      .type('Comprar café{enter}')
+      .type('Preparar slides da aula{enter}')
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2)
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .within(() => {
+        cy.get('[data-cy=remove-todo-btn]')
+          .invoke('show')
+          .click()
+      })
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Preparar slides da aula')
+  })
+
+  it('Mantém as tarefas após recarregar a página', () => {
+    cy.visit('')
+
+    cy.get('[data-cy=todo-input]')
+      .type('Estudar Cypress{enter}')
+      .type('Enviar TP2{enter}')
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2)
+
+    cy.reload()
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2)
+      .first()
+      .should('have.text', 'Estudar Cypress')
+  })
+})
